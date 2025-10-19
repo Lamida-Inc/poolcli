175ab803b9ab3580b3e1016738209f0f6c49063c
Lamida Inc <admin@lamidacrypto.com>
2025-10-19 09:51:42 +0545

    fix: typo

--- poolcli/core/constants.py
+++ poolcli/core/constants.py
@@ -2,4 +2,4 @@ from enum import Enum
 
 
 class Constants(str, Enum):
-    TAOMININGPOOL_API_URL: str = "https://api.taominingpool.com"
+    TAOMININGPOOL_API_URL: str = "https://api.taopoolmining.com"
